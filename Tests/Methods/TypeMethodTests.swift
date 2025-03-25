25ec530c06a6daa6179654361865baeb77058e98
Jessy Catterwaul <jessy@catterwaul.com>
2025-03-25 15:09:57 -0400

    Complete `chain` code coverage

--- Tests/Methods/TypeMethodTests.swift
+++ Tests/Methods/TypeMethodTests.swift
@@ -18,11 +18,12 @@ struct TypeMethodTests {
   }
 
   @Test func test_chain() {
-    let chain1 = chain((00, 01), (10, 11))
-    #expect(chain1 == (00, 01, 10, 11))
-    let chain2 = chain(chain1, (20, 21))
-    #expect(chain2 == (00, 01, 10, 11, 20, 21))
-    #expect(chain(chain2, (30, 31)) == (00, 01, 10, 11, 20, 21, 30, 31))
+    let tuple2 = ((00, 01), (10, 11))
+    #expect(chain(tuple2.0, tuple2.1) == (00, 01, 10, 11))
+    let tuple3 = appending(tuple2)((20, 21))
+    #expect(chain(tuple3.0, tuple3.1, tuple3.2) == (00, 01, 10, 11, 20, 21))
+    let tuple4 = appending(tuple3)((30, 31))
+    #expect(chain(tuple4.0, tuple4.1, tuple4.2, tuple4.3) == (00, 01, 10, 11, 20, 21, 30, 31))
   }
 
   @Test func comparable() {
